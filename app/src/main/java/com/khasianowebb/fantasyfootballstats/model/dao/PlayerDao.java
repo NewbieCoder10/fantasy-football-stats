2513b59995f08e08823a56c2e4c5315ca04b5599
Khasiano <NewbieCoder10>
2019-10-22 16:39:29 -0500

    Updated PlayerDao and TeamDao.

--- app/src/main/java/com/khasianowebb/fantasyfootballstats/model/dao/PlayerDao.java
+++ app/src/main/java/com/khasianowebb/fantasyfootballstats/model/dao/PlayerDao.java
@@ -1,9 +1,36 @@
 package com.khasianowebb.fantasyfootballstats.model.dao;
 
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.khasianowebb.fantasyfootballstats.model.entity.Player;
+import com.khasianowebb.fantasyfootballstats.model.entity.Team;
+import java.util.Collection;
+import java.util.List;
 
 @Dao
 public interface PlayerDao {
+
+  @Insert
+  List<Long> insert(Collection<Player> players);
+
+  @Query("SELECT * FROM Player WHERE player_id = :playerId")
+  LiveData<Player> getByPlayerId(long playerId);
+
+  @Query("SELECT * FROM Player ORDER BY player_id ASC")
+  LiveData<List<Player>> getAllPlayers();
+
+  @Query("SELECT * FROM Player WHERE team_id = :teamId")
+  LiveData<List<Player>> getAllPlayersByTeamId(long teamId);
+
+  @Update
+  int update(Player player);
+
+  @Delete
+  int delete(Player... players);
 
 }
--- app/src/main/java/com/khasianowebb/fantasyfootballstats/model/dao/TeamDao.java
+++ app/src/main/java/com/khasianowebb/fantasyfootballstats/model/dao/TeamDao.java
@@ -1,12 +1,30 @@
 package com.khasianowebb.fantasyfootballstats.model.dao;
 
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 import com.khasianowebb.fantasyfootballstats.model.entity.Team;
+import java.util.Collection;
+import java.util.List;
 
 @Dao
 public interface TeamDao {
+
+  @Insert
+  List<Long> insert(Collection<Team> teams);
+
+  @Query("SELECT * FROM Team WHERE team_id = :teamId")
+  LiveData<Team> getByTeamId(long teamId);
+
+  @Query("SELECT * FROM Team")
+  LiveData<List<Team>> getAllTeams();
+
+  @Update
+  int update(Team team);
 
   @Delete
   int delete(Team... teams);
