9e813522e2e6a90df3861798723e213e3a7ebe4c
Khasiano <NewbieCoder10>
2019-10-23 16:44:49 -0500

    Added Data Model Implementation page to GitHub.

--- app/src/main/java/com/khasianowebb/fantasyfootballstats/controller/MainActivity.java
+++ app/src/main/java/com/khasianowebb/fantasyfootballstats/controller/MainActivity.java
@@ -1,12 +1,17 @@
 package com.khasianowebb.fantasyfootballstats.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import com.khasianowebb.fantasyfootballstats.R;
+import com.khasianowebb.fantasyfootballstats.service.GoogleSignInService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +31,34 @@ public class MainActivity extends AppCompatActivity {
       }
     });
   }
+
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        signOut();
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  private void signOut() {
+    GoogleSignInService.getInstance().signOut()
+        .addOnCompleteListener((task) -> {
+          Intent intent = new Intent(this, LoginActivity.class);
+          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+        });
+  }
+
 
 }
---
+++ docs/datamodelimplementation.md
@@ -0,0 +1,17 @@
+## Data Model Implementation
+
+### Entity Classes
+[`Team`](https://github.com/NewbieCoder10/fantasy-football-stats/blob/master/app/src/main/java/com/khasianowebb/fantasyfootballstats/model/entity/Team.java)
+
+[`Player`](https://github.com/NewbieCoder10/fantasy-football-stats/blob/master/app/src/main/java/com/khasianowebb/fantasyfootballstats/model/entity/Player.java)
+
+### Dao Interfaces
+[`TeamDao`](https://github.com/NewbieCoder10/fantasy-football-stats/blob/master/app/src/main/java/com/khasianowebb/fantasyfootballstats/model/dao/TeamDao.java)
+
+[`PlayerDao`](https://github.com/NewbieCoder10/fantasy-football-stats/blob/master/app/src/main/java/com/khasianowebb/fantasyfootballstats/model/dao/PlayerDao.java)
+
+### Database Class
+[`FantasyFootballStatsDatabase`](https://github.com/NewbieCoder10/fantasy-football-stats/blob/master/app/src/main/java/com/khasianowebb/fantasyfootballstats/service/FantasyFootballStatsDatabase.java)
+
+### Data Definition Language
+[DDL](ddl.md)
