188be88e5be17a17e7db1df9c0b7c8fbb4d7e251
Khasiano <NewbieCoder10>
2019-11-15 13:24:19 -0700

    Updated the MainActivity, need to correct the search-view bar though and also corrected in which way the app opens.

--- app/src/main/java/com/khasianowebb/fantasyfootballstats/controller/MainActivity.java
+++ app/src/main/java/com/khasianowebb/fantasyfootballstats/controller/MainActivity.java
@@ -1,8 +1,13 @@
 package com.khasianowebb.fantasyfootballstats.controller;
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.khasianowebb.fantasyfootballstats.R;
+import com.khasianowebb.fantasyfootballstats.service.GoogleSignInService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,34 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        signOut();
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  private void signOut() {
+    GoogleSignInService.getInstance().signOut()
+        .addOnCompleteListener((task) -> {
+          Intent intent = new Intent(this, LoginActivity.class);
+          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+        });
+  }
+
+
 }
